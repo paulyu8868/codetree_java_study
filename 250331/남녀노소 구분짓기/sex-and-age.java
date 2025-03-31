cc6a0c90695d77753f171d38e8c30f51df350dc4
유수종 <156832213+paulyu8868@users.noreply.github.com>
2025-03-31 10:45:08 +0900

    Title: 남녀노소 구분짓기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250331/남녀노소 구분짓기/sex-and-age.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int sex = Integer.parseInt(sc.nextLine());
+        int age = Integer.parseInt(sc.nextLine());
+
+        if (sex==0){
+            if (age>=19){
+                System.out.print("MAN");
+            }else{
+                System.out.print("BOY");
+            }
+
+        }else{
+        if (age>=19){
+                System.out.print("WOMAN");
+            }else{
+                System.out.print("GIRL");
+            }
+        }
+        }
+    }
+}
