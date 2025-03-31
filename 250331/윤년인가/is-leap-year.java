a8dc544220f4f166bb7a629f025a4ae4c15bd258
유수종 <156832213+paulyu8868@users.noreply.github.com>
2025-03-31 10:50:02 +0900

    Title: 윤년인가, Time: 107ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250331/윤년인가/is-leap-year.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int Y = sc.nextInt();
+        if (Y%4==0){
+            if(Y%100==0&&Y%400!=0){
+                System.out.print("false");
+            }else{
+                System.out.print("true");
+            }
+        }else{
+            System.out.print("false");
+        }
+    }
+}
