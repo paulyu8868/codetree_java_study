fe021abbdfe96c88b6d3a9d4c6dc4cc4ba4e7594
유수종 <156832213+paulyu8868@users.noreply.github.com>
2025-03-31 11:10:18 +0900

    Title: 중앙값 구하기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250331/중앙값 구하기/find-the-median.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int a= sc.nextInt();
+        int b= sc.nextInt();
+        int c= sc.nextInt();
+        int max,min,middle;
+        if (a>b>c||a>c>b){
+            max=a;
+            if (b>c){
+                middle=b;
+                min = c;
+            }else{
+                middle=c;
+                min=b;
+            }
+        }else if(b>a>c||b>a>b){
+            max=b;
+            if (a>c){
+                middle=a;
+                min = c;
+            }else{
+                middle=c;
+                min=a;
+            }
+        }else{
+            max=c;
+            if (a>b){
+                middle=a;
+                min = b;
+            }else{
+                middle=b;
+                min=a;
+            }
+        }
+    }
+}
