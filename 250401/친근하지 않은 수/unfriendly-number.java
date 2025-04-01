70903ba23ca7f209264867769daa5e8b23b8f90a
유수종 <156832213+paulyu8868@users.noreply.github.com>
2025-04-01 15:29:09 +0900

    Title: 친근하지 않은 수, Time: 114ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250401/친근하지 않은 수/unfriendly-number.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int N = Integer.parseInt(sc.nextLine());
+        int cnt=0;
+        for (int i=1;i<=N;i++){
+            if(i%2==0||i%3==0||i%5==0){
+                continue;
+            }
+            cnt++;
+        }
+        System.out.print(cnt);
+    }
+}
